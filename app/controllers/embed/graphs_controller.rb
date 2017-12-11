bb14d1a63e9642b3807f866dbc2a9ed2939706ea
ghuestis <george.huestis@locallabs.com>
2017-12-11 04:10:03 -0600

    rawr

--- app/controllers/embed/graphs_controller.rb
+++ app/controllers/embed/graphs_controller.rb
@@ -1,7 +1,7 @@
 class Embed::GraphsController < ApplicationController
     layout false
 	def show
-		response.headers.delete "X-Frame-Options"
+		response.headers["X-Frame-Options"] = "ALLOW-FROM 24.178.240.237"
     	@graph = Graph.find params[:id]
     end
 end
