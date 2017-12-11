b57e3268b8d4259e9976546a464f269bd9f4dd08
ghuestis <george.huestis@locallabs.com>
2017-12-11 03:09:15 -0600

    remove rack-cors because argh.

--- app/controllers/embed/graphs_controller.rb
+++ app/controllers/embed/graphs_controller.rb
@@ -3,10 +3,13 @@ class Embed::GraphsController < ApplicationController
     layout false
 	def show
     	@graph = Graph.find params[:id]
+    	logger.debug("...")
+    	logger.debug(response.headers)
     end
 
     def allow_iframe_requests
     	logger.debug(response.headers)
     	response.headers.delete "X-Frame-Options" #] = "ALLOWALL"
+    	logger.debug(response.headers)
     end
 end
