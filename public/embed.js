c8809da12c5479865bb8ba0e01ef80027650d279
ghuestis <george.huestis@locallabs.com>
2017-12-11 04:54:26 -0600

    rawr

--- public/embed.js
+++ public/embed.js
@@ -6,6 +6,6 @@ window.onload = function() {
    //iFrame
    var iframe = document.createElement('iframe');
    iframe.style.display = "inherit";
-   iframe.src = "https://graphity.space/embed/" + id;
+   iframe.src = "/embed/" + id;
    document.body.appendChild(iframe);
 };
