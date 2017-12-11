26c158941fec31cb6d30052e3fc3c18c22eb32b9
ghuestis <george.huestis@locallabs.com>
2017-12-11 04:04:13 -0600

    rawr

--- app/controllers/embed/graphs_controller.rb
+++ app/controllers/embed/graphs_controller.rb
@@ -1,7 +1,7 @@
 class Embed::GraphsController < ApplicationController
     layout false
 	def show
-		#response.headers.delete "X-Frame-Options"
+		response.headers.delete "X-Frame-Options"
     	@graph = Graph.find params[:id]
     end
 end
