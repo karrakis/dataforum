560ee5392535311e9e66c9ee06ae855aa3962a2f
ghuestis <george.huestis@locallabs.com>
2017-12-11 02:19:36 -0600

    Rawr

--- app/controllers/embed/graphs_controller.rb
+++ app/controllers/embed/graphs_controller.rb
@@ -1,7 +1,7 @@
 class Embed::GraphsController < ApplicationController
    layout false
    def show
-   	  response.headers["X-Frame-Options"] = "ALLOWALL"
+   	  response.headers.delete "X-Frame-Options" #] = "ALLOWALL"
       @graph = Graph.find params[:id]
    end
 end
