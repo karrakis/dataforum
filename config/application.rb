dbc70d3db37a7cf1475d73775527365a6152e7bd
ghuestis <george.huestis@locallabs.com>
2017-12-11 04:18:42 -0600

    rawr

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module DataForum
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-	  config.middleware.insert_before 0, Rack::Cors do
+	  config.middleware.use Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options, :show]
